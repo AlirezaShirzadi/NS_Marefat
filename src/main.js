9aaf1ad0c1ea5700f5002cee1f49c0bbac95fc27
Alireza Shirzadi <shirzadiwork@gmail.com>
2025-08-17 10:35:05 +0330

    add: home page sections, styles, and assets

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 import './style.css'
 import './../node_modules/preline/dist/preline.js'
 
+
+import $ from 'jquery';
+window.jQuery = window.$ = $;
+
+import { Fancybox } from "@fancyapps/ui/dist/fancybox/";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+Fancybox.bind("[data-fancybox='intro-videos']", {})
