5234da3691824495ade630dd78e2f1b96a6d988b
Alireza Shirzadi <shirzadiwork@gmail.com>
2025-09-16 13:08:17 +0330

    add: placement page

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
                 teen: resolve(__dirname, "teen.html"),
                 ttc: resolve(__dirname, "ttc.html"),
                 teacher: resolve(__dirname, "teacher.html"),
+                placement: resolve(__dirname, "placement.html"),
             },
         },
     },
